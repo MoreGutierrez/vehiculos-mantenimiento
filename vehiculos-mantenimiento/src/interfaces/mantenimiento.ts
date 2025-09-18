5c014f092eaafedcb835dbfe346725206f04326e
MoreGutierrez <morelstuff2000@gmail.com>
2025-09-18 10:36:08 -0300

    creacion de interfaces necesarias

---
+++ vehiculos-mantenimiento/src/interfaces/mantenimiento.ts
@@ -0,0 +1,8 @@
+
+export interface Mantenimiento {
+  id: number;
+  vehiculoId: number;
+  tipo: string;
+  fecha: string;
+  costo: number;
+}
---
+++ vehiculos-mantenimiento/src/interfaces/vehiculo.ts
@@ -0,0 +1,9 @@
+
+export interface Vehiculo {
+  id: number;
+  marca: string;
+  modelo: string;
+  patente: string;
+  vencimientoPatente: string;
+  vencimientoSeguro: string;
+}
