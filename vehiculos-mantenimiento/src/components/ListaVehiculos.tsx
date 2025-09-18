3cf035f916e62afbef94b810adfc9cea1a9d58b9
MoreGutierrez <morelstuff2000@gmail.com>
2025-09-18 11:40:02 -0300

    ListaVehiculos ahora carga datos desde archivo .json

--- vehiculos-mantenimiento/src/components/ListaVehiculos.tsx
+++ vehiculos-mantenimiento/src/components/ListaVehiculos.tsx
@@ -1,28 +1,11 @@
-import React from 'react';
 import type { Vehiculo } from '../interfaces/vehiculo';
+import vehiculos from '../data/arrayVehiculos.json';
 
 
-const ListaVehiculos: React.FC = () => {
-
+function ListaVehiculos() {
+  const listaVehiculos: Vehiculo[] = vehiculos;
   //array de prueba
-  const vehiculos: Vehiculo[] = [
-    {
-      id: 1,
-      marca: 'Toyota',
-      modelo: 'Corolla',
-      patente: 'ABC123',
-      vencimientoPatente: '2025-10-01',
-      vencimientoSeguro: '2025-09-30'
-    },
-    {
-      id: 2,
-      marca: 'Honda',
-      modelo: 'Civic',
-      patente: 'XYZ789',
-      vencimientoPatente: '2025-11-15',
-      vencimientoSeguro: '2025-11-10'
-    }
-  ];
+
 
   return (
     <>
@@ -30,7 +13,7 @@ const ListaVehiculos: React.FC = () => {
         <h2>Lista de Vehiculos</h2>
 
         <ul className="lista-vehiculos">
-          {vehiculos.map(vehiculo => (
+          {listaVehiculos.map(vehiculo => (
             <li key={vehiculo.id}>
               {vehiculo.marca} {vehiculo.modelo} - Patente: {vehiculo.patente} <br />
               {/*br es un salto de línea*/}
